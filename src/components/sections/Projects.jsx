405b55c8eff90b9e3b436feaaa3a3e16f6b2784a
shahbaz <shahbaznadeem654@gmail.com>
2025-03-03 11:43:29 +0500

    home and about page completed

---
+++ src/components/sections/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <section id='project' className='min-h-screen flex items-center justify-center py-20'>
+        <div className="max-w-5xl mx-auto px-4">
+            <h2 className='text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center'>Featured Projects</h2>
+        </div>
+    </section>
+  )
+}
+
+export default Projects
