6d4f3e28acf3bc50eddb9683ddfd984d2010beb3
shahbaz <shahbaznadeem654@gmail.com>
2025-03-04 14:59:00 +0500

    testing

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
                         exceptional performance and a delightful user experience.
                     </p>
                     <div className="flex justify-center space-x-4">
-                        <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded font-medium relative transition-all duration-200 hover:-translate-y-0.5">
+                        <a href="#project" className="bg-blue-500 text-white py-3 px-6 rounded font-medium relative transition-all duration-200 hover:-translate-y-0.5">
                             View Projects
                         </a>
                         <a href="#contact" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10">
