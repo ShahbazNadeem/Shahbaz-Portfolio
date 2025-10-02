e1b4950f4d6e1edf679f5acb8af0d51356ddcf23
shahbaz <shahbaznadeem654@gmail.com>
2025-10-02 16:00:11 +0500

    updated portfolio 2/10/2025

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -3,13 +3,19 @@ import { RevealOnScroll } from './../RevealOnScroll';
 
 const About = () => {
     const frontendSkills = [
-        "React",
+        "React js",
+        "Next js",
         "Vite",
+        "JavaScript",
         "TypeScript",
         "TailwindCSS",
         "Bootstrap",
     ];
-    const backendSkills = ["Node.js", "MongoDB"];
+    const backendSkills = ["Node.js", "MongoDB", "Express js", "JavaScript", "TypeScript",];
+    const databaseManagementSkills = [" MySQL", "MongoDB"];
+    const APIIntegration = ["RESTful APIs", " REST", "FastAPI"];
+    const AIIntegration = ["Open AI", "Claude", "Gemini", "ChatGPT", "Llama"];
+    const VersionControl = ["Git", "GitHub"];
     return (
         <section id='about' className='min-h-screen flex items-center justify-center py-20'>
             <RevealOnScroll>
@@ -19,22 +25,22 @@ const About = () => {
                     <div className="rounded-xl p-8 border-white/10 border-[2px] hover:-translate-y-1 transition-all">
                         <p className='text-gray-300 mb-6'>Passionate developer with expertise in building scalable web applications and creating innovative solutions.</p>
                         <p className='text-gray-300 mb-6'>
-                            ✅ 4+ years of experience <br/>
-                            ✅ Excelling in delivering Fortune 500-scale projects<br/>
-                            👌🏻 100 % Successfully delivered projects<br/><br/>
+                            ✅ 4+ years of experience <br />
+                            ✅ Excelling in delivering Fortune 500-scale projects<br />
+                            👌🏻 100 % Successfully delivered projects<br /><br />
 
-                            Hi there! I'm a Senior Full Stack developer with over 4+ years of experience, I've collaborated with different companies. My expertise spans standalone and API-driven web applications and token-based RestFul API servers. Whether it's cutting-edge technologies or tailored project needs, I have the skills and knowledge to deliver.<br/><br/>
+                            Hi there! I'm a Senior Full Stack developer with over 4+ years of experience, I've collaborated with different companies. My expertise spans standalone and API-driven web applications and token-based RestFul API servers. Whether it's cutting-edge technologies or tailored project needs, I have the skills and knowledge to deliver.<br /><br />
 
-                            Tech-stack:<br/>
-                            ⁃ Front-End Development: React.js, Vue.js, JavaScript, TypeScript, Tailwind CSS, HTML<br/>
-                            ⁃ Back-End Development: Node.js, Express.js, Next.js, Python, Django, Firebase,<br/>
-                            ⁃ Database Management: MySQL, MongoDB<br/>
-                            ⁃ API Integration: RESTful APIs, REST, FastAPI<br/>
-                            ⁃ AI Integration: Open AI, Claude, Gemini, ChatGPT, Llama<br/>
-                            ⁃ Version Control: Git, GitHub<br/><br/>
+                            Tech-stack:<br />
+                            ⁃ Front-End Development: React.js, Vue.js, JavaScript, TypeScript, Tailwind CSS, HTML<br />
+                            ⁃ Back-End Development: Node.js, Express.js, Next.js, Python, Django, Firebase,<br />
+                            ⁃ Database Management: MySQL, MongoDB<br />
+                            ⁃ API Integration: RESTful APIs, REST, FastAPI<br />
+                            ⁃ AI Integration: Open AI, Claude, Gemini, ChatGPT, Llama<br />
+                            ⁃ Version Control: Git, GitHub<br /><br />
 
-                            My Approach<br/>
-                            I believe in clean, maintainable code and seamless user experiences. I deliver fast, with clear communication to help your business grow online.<br/><br/>
+                            My Approach<br />
+                            I believe in clean, maintainable code and seamless user experiences. I deliver fast, with clear communication to help your business grow online.<br /><br />
 
                             Let's connect and talk about what you need — I’m ready to turn your vision into reality.
                         </p>
@@ -54,6 +60,46 @@ const About = () => {
                                 <h3 className='text-xl font-bold mb-4'>Backend</h3>
                                 <div className="flex flex-wrap gap-2">
                                     {backendSkills.map((tech, key) => (
+                                        <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm :bg-blue-500/20  transition">
+                                            {tech}
+                                        </span>
+                                    ))}
+                                </div>
+                            </div>
+                            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                                <h3 className='text-xl font-bold mb-4'>Database Management</h3>
+                                <div className="flex flex-wrap gap-2">
+                                    {databaseManagementSkills.map((tech, key) => (
+                                        <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm :bg-blue-500/20  transition">
+                                            {tech}
+                                        </span>
+                                    ))}
+                                </div>
+                            </div>
+                            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                                <h3 className='text-xl font-bold mb-4'>Database Management</h3>
+                                <div className="flex flex-wrap gap-2">
+                                    {APIIntegration.map((tech, key) => (
+                                        <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm :bg-blue-500/20  transition">
+                                            {tech}
+                                        </span>
+                                    ))}
+                                </div>
+                            </div>
+                            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                                <h3 className='text-xl font-bold mb-4'>Database Management</h3>
+                                <div className="flex flex-wrap gap-2">
+                                    {AIIntegration.map((tech, key) => (
+                                        <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm :bg-blue-500/20  transition">
+                                            {tech}
+                                        </span>
+                                    ))}
+                                </div>
+                            </div>
+                            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                                <h3 className='text-xl font-bold mb-4'>Database Management</h3>
+                                <div className="flex flex-wrap gap-2">
+                                    {VersionControl.map((tech, key) => (
                                         <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm :bg-blue-500/20  transition">
                                             {tech}
                                         </span>
